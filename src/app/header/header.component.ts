1d7f1f0fc13c303393e478dedc825241ec231310
Souleymane Thiongane <mansoulxyz@gmail.com>
2020-03-29 20:54:16 +0000

    Rajout des icones du header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,75 +14,29 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-        label: 'File',
+        label: 'Accueil',
         icon: 'pi pi-fw pi-file',
-        items: [{
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-          items: [
-            { label: 'Project' },
-            { label: 'Other' },
-          ]
-        },
-        { label: 'Open' },
-        { separator: true },
-        { label: 'Quit' }
-        ]
+       
       },
       {
-        label: 'Edit',
+        label: 'Stock',
         icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
-        ]
+       
       },
       {
-        label: 'Help',
+        label: 'Distributeurs',
         icon: 'pi pi-fw pi-question',
-        items: [
-          {
-            label: 'Contents'
-          },
-          {
-            label: 'Search',
-            icon: 'pi pi-fw pi-search',
-            items: [
-              {
-                label: 'Text',
-                items: [
-                  {
-                    label: 'Workspace'
-                  }
-                ]
-              },
-              {
-                label: 'File'
-              }
-            ]
-          }
-        ]
+  
       },
       {
-        label: 'Actions',
+        label: 'Bénéficiaires',
         icon: 'pi pi-fw pi-cog',
-        items: [
-          {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-              { label: 'Save', icon: 'pi pi-fw pi-save' },
-              { label: 'Update', icon: 'pi pi-fw pi-save' },
-            ]
-          },
-          {
-            label: 'Other',
-            icon: 'pi pi-fw pi-tags',
-            items: [
-              { label: 'Delete', icon: 'pi pi-fw pi-minus' }
-            ]
-          }
-        ]
+        
+      },
+      {
+        label: 'Statistiques',
+        icon: 'pi pi-fw pi-cog',
+        
       },
       { separator: true },
       {
