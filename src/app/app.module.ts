e77fe26becff0c31ba56ad218e31013d24050a90
Ngor THIAM <thiam.ngor@gmail.com>
2020-03-29 21:31:53 +0200

    add modules in app and routing using lazy loading.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData, DatePipe } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
 
 /**
- *  PRIMENG
+ * PRIMENG
  */
-
 import { MenubarModule } from 'primeng/menubar';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
@@ -17,6 +20,19 @@ import { FooterComponent } from './footer/footer.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { HelpersModule } from './modules/helpers/helpers.module';
+import { FileService } from './modules/helpers/file/file.service';
+import { RequestCacheService } from './modules/helpers/services/request-cache.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CachingInterceptorService } from './modules/helpers/services/caching-interceptor.service';
+import { BeneficiairesModule } from './components/beneficiaires/beneficiaires.module';
+import { AllocationsModule } from './components/allocations/allocations.module';
+import { BiensModule } from './components/biens/biens.module';
+import { DistributeursModule } from './components/distributeurs/distributeurs.module';
+import { StocksModule } from './components/stocks/stocks.module';
+import { UtilisateursModule } from './components/utilisateurs/utilisateurs.module';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,13 +42,27 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
     MenubarModule,
     InputTextModule,
     ButtonModule,
+    AppRoutingModule,
+    HelpersModule,
+    BeneficiairesModule,
+    BiensModule,
+    AllocationsModule,
+    DistributeursModule,
+    StocksModule,
+    UtilisateursModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [
+    DatePipe,
+    FileService,
+    RequestCacheService,
+    { provide: HTTP_INTERCEPTORS, useClass: CachingInterceptorService, multi: true },
+    { provide: LOCALE_ID, useValue: 'fr-FR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
