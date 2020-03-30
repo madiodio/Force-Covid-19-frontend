f1f940014cca99aec3597fcfe6128334591f9bae
Ngor THIAM <thiam.ngor@gmail.com>
2020-03-30 19:18:52 +0200

    complete init listing of differents module elements.

--- src/app/components/livreurs/list-livreur/list-livreur.component.ts
+++ src/app/components/livreurs/list-livreur/list-livreur.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Livreur } from 'src/app/models/livreur';
+import { LivreurService } from 'src/app/services/livreur.service';
+import { SearchCriteria } from 'src/app/models/search-critaria';
 
 @Component({
   selector: 'app-list-livreur',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListLivreurComponent implements OnInit {
 
-  constructor() { }
+  listItems: Livreur[];
+  cols: any[];
+
+  selectedItems: Livreur[];
+
+  loading: boolean = true;
+  
+  query = new SearchCriteria();
+
+  constructor(private livreurService: LivreurService) { }
 
   ngOnInit(): void {
+
+    this.cols = [
+      { field: 'name', header: 'Nom' },
+      { field: 'manager', header: 'Gestionnaire' },
+      { field: 'numberOfDeliverer', header: 'Nombre de livreur' },
+      { field: 'phoneNumber', header: 'Téléphone' }
+    ];
+    this.livreurService.getLivreurs(this.query);
+    this.listItems = this.livreurService.livreurs;
+    this.loadFakeData();
+  }
+
+  removeItme(id){
+    console.log(id);
+  }
+
+  loadFakeData() {
+    this.listItems = [];
+    for (let i = 1; i <= 20; i++) {
+      let item = new Livreur();
+      item.id = '' + i;
+      item .name= 'Nom ' + i;
+      item.manager = 'Gestionnaire ' + i;
+      item.numberOfDeliverer = '' + i;
+      item.phoneNumber = 'telephone ' + i;
+      this.listItems.push(item);
+    }
+
   }
 
 }
