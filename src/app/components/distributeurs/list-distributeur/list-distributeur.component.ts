f8d9f59f907fb203b21a7df8d5b44e0b5f31bc21
Saliou Gning <saliou.gning@orange-sonatel.com>
2020-04-01 22:20:19 +0200

    Adding filter to other pages

--- src/app/components/distributeurs/list-distributeur/list-distributeur.component.ts
+++ src/app/components/distributeurs/list-distributeur/list-distributeur.component.ts
@@ -5,6 +5,12 @@ import { Distributeur } from 'src/app/models/distributeur';
 import { DistributeurService } from 'src/app/services/distributeur.service';
 import { SearchCriteria } from 'src/app/models/search-critaria';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import {SelectItem} from 'primeng/api';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-list-distributeur',
@@ -32,7 +38,30 @@ export class ListDistributeurComponent implements OnInit, OnDestroy {
 
   errorMsg: any;
 
-  constructor(private distributeurService: DistributeurService, private global: GlobalService) { }
+  cities1: SelectItem[];
+  cities2: SelectItem[];
+  selectedCity1: City;
+  selectedCity2: City;
+
+  constructor(private distributeurService: DistributeurService, private global: GlobalService) {
+    this.cities1 = [
+      {label:'Select City', value:null},
+      {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
+      {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
+      {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
+      {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}},
+      {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
+    ];
+
+    this.cities2 = [
+      {label:'Select City', value:null},
+      {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
+      {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
+      {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
+      {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}},
+      {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
+    ];
+  }
 
   ngOnInit(): void {  
     this.cols = [
--- src/app/components/stocks/list-stock/list-stock.component.ts
+++ src/app/components/stocks/list-stock/list-stock.component.ts
@@ -4,6 +4,12 @@ import { StockService } from 'src/app/services/stock.service';
 import { SearchCriteria } from 'src/app/models/search-critaria';
 import { Subscription, Subject } from 'rxjs';
 import { GlobalService } from 'src/app/global.service';
+import {SelectItem} from 'primeng/api';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-list-stock',
@@ -30,9 +36,33 @@ export class ListStockComponent implements OnInit {
   modalTitle: string;
 
   errorMsg: any;
+
+  cities1: SelectItem[];
+  cities2: SelectItem[];
+  selectedCity1: City;
+  selectedCity2: City;
+
   constructor(
     private stockService: StockService,
-    private global: GlobalService) { }
+    private global: GlobalService) {
+      this.cities1 = [
+        {label:'Select City', value:null},
+        {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
+        {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
+        {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
+        {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}},
+        {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
+      ];
+  
+      this.cities2 = [
+        {label:'Select City', value:null},
+        {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
+        {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
+        {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
+        {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}},
+        {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
+      ];
+    }
 
   ngOnInit(): void {
 
